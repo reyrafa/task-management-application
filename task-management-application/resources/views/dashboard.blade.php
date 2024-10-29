5f401f1563e04540d0d5e20f5c6206b1af7833b3
reyrafa <rafaelcostemiano05@gmail.com>
2024-10-29 08:17:39 +0800

    Dashboard - begin analytics

--- task-management-application/resources/views/dashboard.blade.php
+++ task-management-application/resources/views/dashboard.blade.php
@@ -7,10 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <div class="grid grid-cols-6 gap-10">
+                <div class="bg-white p-5 rounded shadow-lg">
+                    {{ auth()->user()->categories()->count() }}
                 </div>
+                <div class="bg-white">
+                    {{ auth()->user()->tasks()->count() }}
+                </div>
+
             </div>
         </div>
     </div>
